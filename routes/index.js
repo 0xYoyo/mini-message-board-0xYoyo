52f63e6db9db92c00fb7a605e28e265d9e575835
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2023-06-27 20:17:33 +0300

    populate routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amanda",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  const { authorName, messageText } = req.body;
+  messages.push({ text: messageText, user: authorName, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
